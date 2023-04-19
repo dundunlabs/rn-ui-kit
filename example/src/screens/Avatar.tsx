ca408e7a0a13ea6d96e08b5866dadbc2cc3ea9ba
Dung Tran <dung28.td@gmail.com>
2023-04-19 11:28:38 +0700

    Add avatar sizes example

--- example/src/screens/Avatar.tsx
+++ example/src/screens/Avatar.tsx
@@ -1,20 +1,69 @@
-import { Fragment } from 'react'
+import { FlatList, Text, View } from 'react-native'
 import { Avatar } from 'rn-ui-kit'
+
+const data = [
+  {
+    title: 'Variants',
+    data: [
+      {
+        variant: 'circular',
+        uri: 'https://mui.com/static/images/avatar/1.jpg'
+      },
+      {
+        variant: 'rounded',
+        uri: 'https://mui.com/static/images/avatar/1.jpg'
+      },
+      {
+        variant: 'square',
+        uri: 'https://mui.com/static/images/avatar/1.jpg'
+      }
+    ]
+  },
+  {
+    title: 'Sizes',
+    data: [
+      {
+        size: 'sm',
+        uri: 'https://mui.com/static/images/avatar/1.jpg'
+      },
+      {
+        size: 'md',
+        uri: 'https://mui.com/static/images/avatar/1.jpg'
+      },
+      {
+        size: 'lg',
+        uri: 'https://mui.com/static/images/avatar/1.jpg'
+      }
+    ]
+  }
+] as const
 
 export default function AvatarScreen() {
   return (
-    <Fragment>
-      <Avatar
-        source={{ uri: 'https://mui.com/static/images/avatar/1.jpg' }}
-      />
-      <Avatar
-        variant='rounded'
-        source={{ uri: 'https://mui.com/static/images/avatar/2.jpg' }}
-      />
-      <Avatar
-        variant='square'
-        source={{ uri: 'https://mui.com/static/images/avatar/3.jpg' }}
-      />
-    </Fragment>
+    <FlatList
+      data={data}
+      renderItem={({ item }) => {
+        const { title, data } = item
+
+        return (
+          <View>
+            <Text>{title}</Text>
+            <View style={{ flexDirection: 'row' }}>
+              {data.map(({ uri, ...attrs }, idx) => {
+
+                return (
+                  <Avatar
+                    key={idx}
+                    variant={'variant' in attrs ? attrs.variant : 'circular'}
+                    size={'size' in attrs ? attrs.size : 'md'}
+                    source={{ uri }}
+                  />
+                )
+              })}
+            </View>
+          </View>
+        )
+      }}
+    />
   )
 }
